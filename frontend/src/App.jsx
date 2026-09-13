1ea8dd61eaf7ef8a425031930a05227c8169b6f0
Gian <Cordxop@gmail.com>
2026-09-13 17:57:10 -0500

    Update App.jsx
    Import statement page fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import ContractsPage from './pages/ContractsPage.jsx';
 import ContractAnalysisPage from './pages/ContractAnalysisPage.jsx';
 import CoachRostersPage from './pages/CoachRostersPage.jsx';
 import ComplianceDashboardPage from './pages/ComplianceDashboardPage.jsx';
+import ComplianceOfficerDashboardPage from './pages/ComplianceOfficerDashboardPage.jsx';
 import './styles/global.css';
 import './App.css';
 
